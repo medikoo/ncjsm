34469ed33960b78a33d56c1e750b7c7746ae9e0f
Mariusz Nowak <medyk@medikoo.com>
2015-07-24 15:56:46 +0200

    isPackageRoot utility

---
+++ is-package-root.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var ensureString = require('es5-ext/object/validate-stringifiable-value')
+  , deferred     = require('deferred')
+  , resolve      = require('path').resolve
+  , stat         = require('fs2/stat');
+
+module.exports = function (path) {
+	path = ensureString(path);
+	return deferred.some([
+		stat(resolve(path, 'package.json'))(function (stats) { return stats.isFile(); }, function (e) {
+			if (e.code === 'ENOENT') return false;
+			throw e;
+		}),
+		stat(resolve(path, 'node_modules'))(function (stats) {
+			return stats.isDirectory();
+		}, function (e) {
+			if (e.code === 'ENOENT') return false;
+			throw e;
+		})
+	]);
+};
---
+++ test/is-package-root.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var deferred = require('deferred')
+  , path     = require('path')
+
+  , resolve = path.resolve
+
+  , playgroundDir = resolve(__dirname, '__playground');
+
+module.exports = function (t, a, d) {
+	deferred(
+		t(playgroundDir)(function (value) { a(value, true, "node_modules"); }),
+		t(resolve(playgroundDir, 'otherdir'))(function (value) { a(value, false, "Empty"); }),
+		t(resolve(playgroundDir, 'dir'))(function (value) { a(value, true, "package.json"); }),
+		t(resolve(playgroundDir, 'node_modules/outer'))(function (value) { a(value, true,
+			"package.json and node_modules"); }),
+		t(resolve(playgroundDir, 'node_modules/outer3'))(function (value) { a(value, false,
+			"In node_modules"); })
+	).done(function () { d(); }, d);
+};
