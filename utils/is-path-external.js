eb903a0e8e4b11c98f78f9a9a07cecba03433a31
Mariusz Nowak <medyk@medikoo.com>
2015-09-02 11:27:38 +0200

    isPathExternal utility

---
+++ utils/is-path-external.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var ensureString = require('es5-ext/object/validate-stringifiable-value');
+
+module.exports = function (path) {
+	var pathChar;
+	path = ensureString(path);
+	pathChar = path.charAt(0);
+	if (pathChar === '/') return false;
+	if (pathChar === '.') {
+		pathChar = path.charAt(1);
+		if (!pathChar || (pathChar === '/')) return false;
+		if (pathChar === '.') {
+			pathChar = path.charAt(2);
+			if (!pathChar || (pathChar === '/')) return false;
+		}
+	}
+	return true;
+};
