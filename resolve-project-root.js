f43108ca7bad458fdcf4a6ea525f37eb357e6ea2
Mariusz Nowak <medyk@medikoo.com>
2015-09-02 13:04:28 +0200

    Seclude resolveRoot logic

--- resolve-project-root.js
+++ resolve-project-root.js
@@ -2,27 +2,6 @@
 
 'use strict';
 
-var ensureValue   = require('es5-ext/object/valid-value')
-  , deferred      = require('deferred')
-  , stat          = require('fs2/stat')
-  , path          = require('path')
-  , isPackageRoot = require('./is-package-root')
+var resolveRoot = require('./lib/resolve-root');
 
-  , resolve = path.resolve, sep = path.sep;
-
-module.exports = function (path) {
-	path = resolve(String(ensureValue(path)));
-	return stat(path)(function (stats) {
-		var tokens;
-		if (!stats.isDirectory()) throw new Error("Provided path is not a directory path");
-		tokens = path.split(sep);
-		tokens.forEach(function (token, index) {
-			if (!index) return;
-			tokens[index] = tokens[index - 1] + sep + token;
-		});
-		tokens[0] = tokens[0] + sep;
-		return deferred.find(tokens, function (path) { return isPackageRoot(path); })(function (path) {
-			return (path === undefined) ? null : path;
-		});
-	});
-};
+module.exports = function (path) { return resolveRoot(path, true); };
