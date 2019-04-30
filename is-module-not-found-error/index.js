8b2e641e5120e1274b0afb9a689373d7c868c41c
Mariusz Nowak <medyk@medikoo.com>
2019-04-30 13:52:53 +0200

    feat: improve validation in isModuleNotFoundError

--- is-module-not-found-error/index.js
+++ is-module-not-found-error/index.js
@@ -3,6 +3,8 @@
 
 "use strict";
 
+var ensureString = require("type/string/ensure");
+
 var pathToken = ":path", pattern;
 
 var resolveMessage = function (error) {
@@ -14,5 +16,7 @@ try { require(pathToken); }
 catch (error) { pattern = resolveMessage(error); }
 
 module.exports = function (error, path) {
+	path = ensureString(path);
+	if (!error || typeof error.message !== "string") return false;
 	return resolveMessage(error) === pattern.replace(pathToken, path);
 };
