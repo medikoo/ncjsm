6f20188ae44b198312ae2c5bbddf7f6711334fea
Mariusz Nowak <mariusz@medikoo.com>
2019-08-30 15:01:24 +0200

    test: Refactor to async/await

--- test/resolve/index.js
+++ test/resolve/index.js
@@ -1,85 +1,64 @@
 "use strict";
 
-const deferred    = require("deferred")
-    , { resolve } = require("path");
+const { resolve } = require("path");
 
 const pgDir = resolve(__dirname, "../__playground");
 
-module.exports = function (t, a, d) {
-	deferred(
-		t(pgDir, "./foo").then(value => { a(value, resolve(`${ pgDir }/foo.js`)); }),
-		t(pgDir, "./foo.js").then(value => { a(value, resolve(`${ pgDir }/foo.js`)); }),
-		t(pgDir, "./foo.json").then(value => { a(value, null); }),
-		t(pgDir, "./other").then(value => { a(value, resolve(`${ pgDir }/other.js`)); }),
-		t(pgDir, "./other/").then(value => { a(value, resolve(`${ pgDir }/other/index.js`)); }),
-		t(pgDir, "./samename").then(value => { a(value, resolve(`${ pgDir }/samename`)); }),
-		t(pgDir, "./samename.js").then(value => { a(value, resolve(`${ pgDir }/samename.js`)); }),
-		t(pgDir, "./samename.json").then(value => {
-			a(value, resolve(`${ pgDir }/samename.json`));
-		}),
-		t(pgDir, "./samename").then(value => { a(value, resolve(`${ pgDir }/samename`)); }),
-		t(pgDir, "./dir").then(value => { a(value, resolve(`${ pgDir }/dir/lorem.js`)); }),
-		t(pgDir, "./dir/lorem").then(value => { a(value, resolve(`${ pgDir }/dir/lorem.js`)); }),
-		t(pgDir, "./dir/subdir/bar").then(value => {
-			a(value, resolve(`${ pgDir }/dir/subdir/bar.js`));
-		}),
-		t(`${ pgDir }/dir`, ".").then(value => { a(value, resolve(`${ pgDir }/dir/lorem.js`)); }),
-		t(`${ pgDir }/dir`, "./").then(value => { a(value, resolve(`${ pgDir }/dir/lorem.js`)); }),
-		t(`${ pgDir }/dir`, "./lorem").then(value => {
-			a(value, resolve(`${ pgDir }/dir/lorem.js`));
-		}),
-		t(`${ pgDir }/dir`, "../other").then(value => {
-			a(value, resolve(`${ pgDir }/other.js`));
-		}),
-		t(`${ pgDir }/dir`, "../other/").then(value => {
-			a(value, resolve(`${ pgDir }/other/index.js`));
-		}),
-		t(`${ pgDir }/dir/subdir`, "../").then(value => {
-			a(value, resolve(`${ pgDir }/dir/lorem.js`));
-		}),
-		t(`${ pgDir }/dir/subdir`, "../../foo").then(value => {
-			a(value, resolve(`${ pgDir }/foo.js`));
-		}),
-		t(pgDir, "outer").then(value => {
-			a(value, resolve(`${ pgDir }/node_modules/outer/raz.js`));
-		}),
-		t(pgDir, "outer/boo").then(value => {
-			a(value, resolve(`${ pgDir }/node_modules/outer/boo.js`));
-		}),
-		t(pgDir, "outer/boo.json").then(value => { a(value, null); }),
-		t(pgDir, "outer3").then(value => {
-			a(value, resolve(`${ pgDir }/node_modules/outer3/index.js`));
-		}),
-		t(pgDir, "pkg-main-dir").then(value => {
-			a(value, resolve(`${ pgDir }/node_modules/pkg-main-dir/lib/index.js`));
-		}),
-		t(pgDir, "nested/elo").then(value => { a(value, null); }),
-		t(`${ pgDir }/node_modules/outer`, "outer3").then(value => {
-			a(value, resolve(`${ pgDir }/node_modules/outer3/index.js`));
-		}),
-		t(`${ pgDir }/node_modules/outer`, "project/foo").then(value => { a(value, null); }),
-		t(`${ pgDir }/node_modules/outer`, "nested/elo").then(value => {
-			a(value, resolve(`${ pgDir }/node_modules/outer/node_modules/nested/elo.js`));
-		}),
-		t(`${ pgDir }/node_modules/outer/node_modules/nested`, "project/foo").then(value => {
-			a(value, null);
-		}),
-		t(`${ pgDir }/node_modules/outer/node_modules/nested`, "outer").then(value => {
-			a(value, resolve(`${ pgDir }/node_modules/outer/raz.js`));
-		}),
-		t(`${ pgDir }/node_modules/outer/node_modules/nested`, "outer/boo").then(value => {
-			a(value, resolve(`${ pgDir }/node_modules/outer/boo.js`));
-		}),
-		t(`${ pgDir }/node_modules/outer/node_modules/nested`, "outer3").then(value => {
-			a(value, resolve(`${ pgDir }/node_modules/outer3/index.js`));
-		}),
+module.exports = async (t, a) => {
+	a(await t(pgDir, "./foo"), resolve(`${ pgDir }/foo.js`));
+	a(await t(pgDir, "./foo.js"), resolve(`${ pgDir }/foo.js`));
+	a(await t(pgDir, "./foo.json"), null);
+	a(await t(pgDir, "./other"), resolve(`${ pgDir }/other.js`));
+	a(await t(pgDir, "./other/"), resolve(`${ pgDir }/other/index.js`));
+	a(await t(pgDir, "./samename"), resolve(`${ pgDir }/samename`));
+	a(await t(pgDir, "./samename.js"), resolve(`${ pgDir }/samename.js`));
+	a(await t(pgDir, "./samename.json"), resolve(`${ pgDir }/samename.json`));
+	a(await t(pgDir, "./samename"), resolve(`${ pgDir }/samename`));
+	a(await t(pgDir, "./dir"), resolve(`${ pgDir }/dir/lorem.js`));
+	a(await t(pgDir, "./dir/lorem"), resolve(`${ pgDir }/dir/lorem.js`));
+	a(await t(pgDir, "./dir/subdir/bar"), resolve(`${ pgDir }/dir/subdir/bar.js`));
+	a(await t(`${ pgDir }/dir`, "."), resolve(`${ pgDir }/dir/lorem.js`));
+	a(await t(`${ pgDir }/dir`, "./"), resolve(`${ pgDir }/dir/lorem.js`));
+	a(await t(`${ pgDir }/dir`, "./lorem"), resolve(`${ pgDir }/dir/lorem.js`));
+	a(await t(`${ pgDir }/dir`, "../other"), resolve(`${ pgDir }/other.js`));
+	a(await t(`${ pgDir }/dir`, "../other/"), resolve(`${ pgDir }/other/index.js`));
+	a(await t(`${ pgDir }/dir/subdir`, "../"), resolve(`${ pgDir }/dir/lorem.js`));
+	a(await t(`${ pgDir }/dir/subdir`, "../../foo"), resolve(`${ pgDir }/foo.js`));
+	a(await t(pgDir, "outer"), resolve(`${ pgDir }/node_modules/outer/raz.js`));
+	a(await t(pgDir, "outer/boo"), resolve(`${ pgDir }/node_modules/outer/boo.js`));
+	a(await t(pgDir, "outer/boo.json"), null);
+	a(await t(pgDir, "outer3"), resolve(`${ pgDir }/node_modules/outer3/index.js`));
+	a(await t(pgDir, "pkg-main-dir"), resolve(`${ pgDir }/node_modules/pkg-main-dir/lib/index.js`));
+	a(await t(pgDir, "nested/elo"), null);
+	a(
+		await t(`${ pgDir }/node_modules/outer`, "outer3"),
+		resolve(`${ pgDir }/node_modules/outer3/index.js`)
+	);
+	a(await t(`${ pgDir }/node_modules/outer`, "project/foo"), null);
+	a(
+		await t(`${ pgDir }/node_modules/outer`, "nested/elo"),
+		resolve(`${ pgDir }/node_modules/outer/node_modules/nested/elo.js`)
+	);
+	a(await t(`${ pgDir }/node_modules/outer/node_modules/nested`, "project/foo"), null);
+	a(
+		await t(`${ pgDir }/node_modules/outer/node_modules/nested`, "outer"),
+		resolve(`${ pgDir }/node_modules/outer/raz.js`)
+	);
+	a(
+		await t(`${ pgDir }/node_modules/outer/node_modules/nested`, "outer/boo"),
+		resolve(`${ pgDir }/node_modules/outer/boo.js`)
+	);
+	a(
+		await t(`${ pgDir }/node_modules/outer/node_modules/nested`, "outer3"),
+		resolve(`${ pgDir }/node_modules/outer3/index.js`)
+	);
 
-		// Symlink tests
-		t(pgDir, "./valid-link").then(value => { a(value, resolve(`${ pgDir }/valid-link.js`)); }),
-		t(pgDir, "./deep-link").then(value => { a(value, resolve(`${ pgDir }/deep-link.js`)); }),
-		t(pgDir, "./invalid-link").then(value => a(value, null)),
-		t(pgDir, "./invalid-link-with-a-fallback").then(value => {
-			a(value, resolve(`${ pgDir }/invalid-link-with-a-fallback.json`));
-		})
-	).done(() => { d(); }, d);
+	// Symlink tests
+	a(await t(pgDir, "./valid-link"), resolve(`${ pgDir }/valid-link.js`));
+	a(await t(pgDir, "./deep-link"), resolve(`${ pgDir }/deep-link.js`));
+	a(await t(pgDir, "./invalid-link"), null);
+	a(
+		await t(pgDir, "./invalid-link-with-a-fallback"),
+		resolve(`${ pgDir }/invalid-link-with-a-fallback.json`)
+	);
 };
