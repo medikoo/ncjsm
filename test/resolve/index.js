523498a9b216af4e2f7c63177fdb28255dfb7191
Mariusz Nowak <mariusz@medikoo.com>
2019-08-30 15:02:06 +0200

    test: Improve var name

--- test/resolve/index.js
+++ test/resolve/index.js
@@ -2,63 +2,66 @@
 
 const { resolve } = require("path");
 
-const pgDir = resolve(__dirname, "../__playground");
+const playgroundDir = resolve(__dirname, "../__playground");
 
 module.exports = async (t, a) => {
-	a(await t(pgDir, "./foo"), resolve(`${ pgDir }/foo.js`));
-	a(await t(pgDir, "./foo.js"), resolve(`${ pgDir }/foo.js`));
-	a(await t(pgDir, "./foo.json"), null);
-	a(await t(pgDir, "./other"), resolve(`${ pgDir }/other.js`));
-	a(await t(pgDir, "./other/"), resolve(`${ pgDir }/other/index.js`));
-	a(await t(pgDir, "./samename"), resolve(`${ pgDir }/samename`));
-	a(await t(pgDir, "./samename.js"), resolve(`${ pgDir }/samename.js`));
-	a(await t(pgDir, "./samename.json"), resolve(`${ pgDir }/samename.json`));
-	a(await t(pgDir, "./samename"), resolve(`${ pgDir }/samename`));
-	a(await t(pgDir, "./dir"), resolve(`${ pgDir }/dir/lorem.js`));
-	a(await t(pgDir, "./dir/lorem"), resolve(`${ pgDir }/dir/lorem.js`));
-	a(await t(pgDir, "./dir/subdir/bar"), resolve(`${ pgDir }/dir/subdir/bar.js`));
-	a(await t(`${ pgDir }/dir`, "."), resolve(`${ pgDir }/dir/lorem.js`));
-	a(await t(`${ pgDir }/dir`, "./"), resolve(`${ pgDir }/dir/lorem.js`));
-	a(await t(`${ pgDir }/dir`, "./lorem"), resolve(`${ pgDir }/dir/lorem.js`));
-	a(await t(`${ pgDir }/dir`, "../other"), resolve(`${ pgDir }/other.js`));
-	a(await t(`${ pgDir }/dir`, "../other/"), resolve(`${ pgDir }/other/index.js`));
-	a(await t(`${ pgDir }/dir/subdir`, "../"), resolve(`${ pgDir }/dir/lorem.js`));
-	a(await t(`${ pgDir }/dir/subdir`, "../../foo"), resolve(`${ pgDir }/foo.js`));
-	a(await t(pgDir, "outer"), resolve(`${ pgDir }/node_modules/outer/raz.js`));
-	a(await t(pgDir, "outer/boo"), resolve(`${ pgDir }/node_modules/outer/boo.js`));
-	a(await t(pgDir, "outer/boo.json"), null);
-	a(await t(pgDir, "outer3"), resolve(`${ pgDir }/node_modules/outer3/index.js`));
-	a(await t(pgDir, "pkg-main-dir"), resolve(`${ pgDir }/node_modules/pkg-main-dir/lib/index.js`));
-	a(await t(pgDir, "nested/elo"), null);
+	a(await t(playgroundDir, "./foo"), resolve(`${ playgroundDir }/foo.js`));
+	a(await t(playgroundDir, "./foo.js"), resolve(`${ playgroundDir }/foo.js`));
+	a(await t(playgroundDir, "./foo.json"), null);
+	a(await t(playgroundDir, "./other"), resolve(`${ playgroundDir }/other.js`));
+	a(await t(playgroundDir, "./other/"), resolve(`${ playgroundDir }/other/index.js`));
+	a(await t(playgroundDir, "./samename"), resolve(`${ playgroundDir }/samename`));
+	a(await t(playgroundDir, "./samename.js"), resolve(`${ playgroundDir }/samename.js`));
+	a(await t(playgroundDir, "./samename.json"), resolve(`${ playgroundDir }/samename.json`));
+	a(await t(playgroundDir, "./samename"), resolve(`${ playgroundDir }/samename`));
+	a(await t(playgroundDir, "./dir"), resolve(`${ playgroundDir }/dir/lorem.js`));
+	a(await t(playgroundDir, "./dir/lorem"), resolve(`${ playgroundDir }/dir/lorem.js`));
+	a(await t(playgroundDir, "./dir/subdir/bar"), resolve(`${ playgroundDir }/dir/subdir/bar.js`));
+	a(await t(`${ playgroundDir }/dir`, "."), resolve(`${ playgroundDir }/dir/lorem.js`));
+	a(await t(`${ playgroundDir }/dir`, "./"), resolve(`${ playgroundDir }/dir/lorem.js`));
+	a(await t(`${ playgroundDir }/dir`, "./lorem"), resolve(`${ playgroundDir }/dir/lorem.js`));
+	a(await t(`${ playgroundDir }/dir`, "../other"), resolve(`${ playgroundDir }/other.js`));
+	a(await t(`${ playgroundDir }/dir`, "../other/"), resolve(`${ playgroundDir }/other/index.js`));
+	a(await t(`${ playgroundDir }/dir/subdir`, "../"), resolve(`${ playgroundDir }/dir/lorem.js`));
+	a(await t(`${ playgroundDir }/dir/subdir`, "../../foo"), resolve(`${ playgroundDir }/foo.js`));
+	a(await t(playgroundDir, "outer"), resolve(`${ playgroundDir }/node_modules/outer/raz.js`));
+	a(await t(playgroundDir, "outer/boo"), resolve(`${ playgroundDir }/node_modules/outer/boo.js`));
+	a(await t(playgroundDir, "outer/boo.json"), null);
+	a(await t(playgroundDir, "outer3"), resolve(`${ playgroundDir }/node_modules/outer3/index.js`));
 	a(
-		await t(`${ pgDir }/node_modules/outer`, "outer3"),
-		resolve(`${ pgDir }/node_modules/outer3/index.js`)
+		await t(playgroundDir, "pkg-main-dir"),
+		resolve(`${ playgroundDir }/node_modules/pkg-main-dir/lib/index.js`)
 	);
-	a(await t(`${ pgDir }/node_modules/outer`, "project/foo"), null);
+	a(await t(playgroundDir, "nested/elo"), null);
 	a(
-		await t(`${ pgDir }/node_modules/outer`, "nested/elo"),
-		resolve(`${ pgDir }/node_modules/outer/node_modules/nested/elo.js`)
+		await t(`${ playgroundDir }/node_modules/outer`, "outer3"),
+		resolve(`${ playgroundDir }/node_modules/outer3/index.js`)
 	);
-	a(await t(`${ pgDir }/node_modules/outer/node_modules/nested`, "project/foo"), null);
+	a(await t(`${ playgroundDir }/node_modules/outer`, "project/foo"), null);
 	a(
-		await t(`${ pgDir }/node_modules/outer/node_modules/nested`, "outer"),
-		resolve(`${ pgDir }/node_modules/outer/raz.js`)
+		await t(`${ playgroundDir }/node_modules/outer`, "nested/elo"),
+		resolve(`${ playgroundDir }/node_modules/outer/node_modules/nested/elo.js`)
+	);
+	a(await t(`${ playgroundDir }/node_modules/outer/node_modules/nested`, "project/foo"), null);
+	a(
+		await t(`${ playgroundDir }/node_modules/outer/node_modules/nested`, "outer"),
+		resolve(`${ playgroundDir }/node_modules/outer/raz.js`)
 	);
 	a(
-		await t(`${ pgDir }/node_modules/outer/node_modules/nested`, "outer/boo"),
-		resolve(`${ pgDir }/node_modules/outer/boo.js`)
+		await t(`${ playgroundDir }/node_modules/outer/node_modules/nested`, "outer/boo"),
+		resolve(`${ playgroundDir }/node_modules/outer/boo.js`)
 	);
 	a(
-		await t(`${ pgDir }/node_modules/outer/node_modules/nested`, "outer3"),
-		resolve(`${ pgDir }/node_modules/outer3/index.js`)
+		await t(`${ playgroundDir }/node_modules/outer/node_modules/nested`, "outer3"),
+		resolve(`${ playgroundDir }/node_modules/outer3/index.js`)
 	);
 
 	// Symlink tests
-	a(await t(pgDir, "./valid-link"), resolve(`${ pgDir }/valid-link.js`));
-	a(await t(pgDir, "./deep-link"), resolve(`${ pgDir }/deep-link.js`));
-	a(await t(pgDir, "./invalid-link"), null);
+	a(await t(playgroundDir, "./valid-link"), resolve(`${ playgroundDir }/valid-link.js`));
+	a(await t(playgroundDir, "./deep-link"), resolve(`${ playgroundDir }/deep-link.js`));
+	a(await t(playgroundDir, "./invalid-link"), null);
 	a(
-		await t(pgDir, "./invalid-link-with-a-fallback"),
-		resolve(`${ pgDir }/invalid-link-with-a-fallback.json`)
+		await t(playgroundDir, "./invalid-link-with-a-fallback"),
+		resolve(`${ playgroundDir }/invalid-link-with-a-fallback.json`)
 	);
 };
